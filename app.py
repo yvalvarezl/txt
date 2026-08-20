ee300797af45fe3e1f1e685140582eea87d7465f
Yoselin Alvarez <yvalvarezl@gmail.com>
2026-08-20 08:19:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from gtts import gTTS
 from PIL import Image
 import base64
 
-st.title("Asistente para mudos")
+st.title("Asistente de voz para comunicación")
 image = Image.open('gato_raton.png')
 st.image(image, width=350)
 with st.sidebar:
